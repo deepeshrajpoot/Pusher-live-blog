231960143f434591818e98d26f33befecc3c877d
deepeshrajpoot <deepesh14singh@gmail.com>
2019-05-04 16:31:52 +0530

    implemented-pusher

---
+++ config/initializers/pusher.rb
@@ -0,0 +1,8 @@
+require 'pusher'
+
+Pusher.app_id = '764673'
+Pusher.key = 'bc80f6bcf9e901a13ec3'
+Pusher.secret = '470d7024ee5a883fbe18'
+Pusher.cluster = 'ap2'
+Pusher.logger = Rails.logger
+Pusher.encrypted = true
